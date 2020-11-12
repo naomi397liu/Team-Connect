4233307d295fdb5e1dec162e0a07f5de6ed81a3b
Naomi Liu <nhliu@ucdavis.edu>
2020-11-12 16:36:10 +0000

    added base.html and flash message capabilities

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash
 import crud
 from jinja2 import StrictUndefined
 
@@ -9,22 +9,20 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def login():
     """Show hello.html template."""
-
     return render_template('homepage.html')
 
 @app.route('/createuser')
 def create_user():
     """Show greet.html template """
-
+    # user = create_player(user, pw, bio, sport, city)
     return render_template('createuser.html')
 
 @app.route('/nav')
 def navigate():
     """ Show Navigation page """
-
     return render_template("nav.html")
 
-# @app.route('/users')
+# @app.route('/users', methods=["POST"])
 # def users():
 #     """Show profile.html template """
 #     users = crud.get_players()
