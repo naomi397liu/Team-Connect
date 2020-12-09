12d857b39939f1221a77fe084755202e926e8410
Naomi Liu <nhliu@ucdavis.edu>
2020-12-09 02:01:40 +0000

    Completed walk through of features and furture features

--- README.md
+++ README.md
@@ -6,12 +6,12 @@
 
 Team Connect is an application that unites users with the same athletic interest and who live in the same city, so that they can connect with one another to play casually. If a user has an athletic interest such as soccer, it allows the user to find others near by who would also like to play soccer. They can choose to be on the same team or play against each other. 
 
-## Contents:
+# Contents:
 * [Tech Stack](#tech-stack)
 * [Features](#features)
 * [Future Features](#future)
 
-## <a name="tech-stack"></a>Technologies
+# <a name="tech-stack"></a>Technologies
 - Python
 - JavaScript
 - AJAX
@@ -24,49 +24,57 @@ Team Connect is an application that unites users with the same athletic interest
 - CSS
 - Bootstrap
 
-## <a name="features"></a>Features
+# <a name="features"></a>Features
 
 
-### Start By Creating An Account
+## Start By Creating An Account
 To participate, an individual must first become a user by creating a account. Their username must be unique. They can input a password, preferred sport and location. They can also include a blurb describing themselves as a player but it is limited to 50 characters. 
+
 ![View](https://media.giphy.com/media/P6YNDwhjvZo3XR1tXV/giphy.gif)
 
 
-### Views
+## Views
 Once a user logs in, their profile pops up. They can also view all the users and all teams. They can also view potential teammates who share their sport and city. Potential teams are also teams that share the users sport and city. To see more details, a user can click on another user to view their profile. They can also click on a team to view more details about that team.
+
 ![View](https://media.giphy.com/media/7TptAgICyQMn7KsI8q/giphy.gif)
 
 
-### Creating a Team
-A user can create a team, making them the team captain. They must input a unique team name. Their phone number is displayed to all the users in order to arrange games against other teams through the captain. 
+## Creating a Team
+A user can create a team, making them the team captain. They must input a unique team name, a phone number, sport, city and details about the team. In the description and details portion, the captain can specify what day they would like to play or include a park they would like to play at. A phone number is needed so that users outside of the team can contact the captain to arrange games. 
 
-### Data
+![Create-Team](https://media.giphy.com/media/oDV0Z3FAIP8KJ8SZyk/giphy.gif)
 
-**Built in sports and cities**
 
-- A user contains data on what their sport of interest is and their city
-- A team contains data on their sport, city and team captain AKA the creator of the team 
-- A player creates a many to many relationship through a linked table between teams and users
+Once the team has been created, any user can view its details. The captain is automatically listed in the team's details as well as on the players list. Each user on the player's list is clickable to view their information
 
-### Roadmap 
+![View-Team](https://media.giphy.com/media/uNCXY85sEBzzYZsUwo/giphy.gif)
 
-- [x] Allow users to create profiles in order to be linked up with others that share their location and sports interest(MVP)
-- [x] Allow users to create teams that are added to the database
-- [x] Allow users to join teams
-- [x] Allow users to leave teams
-- [x] Allow teammates to see each other's phone numbers to contact one another
-- [x] Allow any user to view a team captain's phone number to arrange games against that team
-- [x] Allow users to view potential teammates
-- [x] Allow users to view potential teams they could join
 
-**Version 2.0: Future Features**
-- [ ] Separate testdb
-- [ ] JS testing
+## Joining a Team
+Not everyone wants the responsibility of hosting a team, so a user can join a team as well. A user can view the captain's phone number before joining the team and make any inquiries about the team. Once the user joins the team, they can view any teammate's phone number to contact them for example about carpooling. 
+
+### View Before Joining
+
+![Before-Join](https://media.giphy.com/media/reGkKX3NPLNdk7wOks/giphy.gif)
+
+
+### View After Joining
+
+![After-join](https://media.giphy.com/media/9wGhax7qU0hVkAZJzh/giphy.gif)
+
+### Leave
+A user can also leave a team after joining by clicking the same button, but they will still have to input the phone number to leave. 
+
+![Leave](https://media.giphy.com/media/tw5Yvki11i2NenAPDb/giphy.gif)
+
+
+# <a name="future"></a> Future Features
 - [ ] Allow a team captain to delete their team, which will automatically delete all the players from it as well
 - [ ] If a captain removes themselves from the team, the team will be deleted
-- [ ] Importing parks API that support a given sport and provide the city it correlates to
+- [ ] Allow for wider range of city and sport choices
+- [ ] Provide park choice or import database of parks
 - [ ] Add a request game option that allows the captain to choose an opponent team, park and time/date to play at
 - [ ] Add an in chat app instead of phone numbers
-- [ ] Add twilio api to send notification when a game is coming up or when their is a message in their inbox
+- [ ] Add nofication API to send notification when a game is coming up or when their is a message in their inbox
 
 
--- seed_databased.py
+++ seed_databased.py
@@ -52,24 +52,65 @@ def load_test():
         sport = crud.get_sport_by_id(randint(1,len(sports)))
         city = crud.get_city_by_id(randint(1,len(sports)))
         crud.create_user(username, password, bio, sport, city)
-
-    crud.create_team('Killers', 'Play to win',crud.get_user_by_id(1), crud.get_sport_by_id(1), crud.get_city_by_id(1))
-    crud.create_team('Wombats', 'Best D, greatest offense', crud.get_user_by_id(4),crud.get_sport_by_id(2), crud.get_city_by_id(2))
-    crud.create_team('Cougars', 'Come play on Saturdays!', crud.get_user_by_id(7),crud.get_sport_by_id(1), crud.get_city_by_id(2))
-    crud.create_team('Wolves', 'Play ball with me on Tuesday nights!', crud.get_user_by_id(10),crud.get_sport_by_id(2), crud.get_city_by_id(1))
-    crud.create_team('Sharks', 'Speedsters! Playing every weekend!', crud.get_user_by_id(20),crud.get_sport_by_id(3), crud.get_city_by_id(3))
-    crud.create_team('Banana Slugs', 'Play to win',crud.get_user_by_id(1), crud.get_sport_by_id(1), crud.get_city_by_id(1))
-    crud.create_team('Peach Eaters', 'Best D, greatest offense', crud.get_user_by_id(4),crud.get_sport_by_id(2), crud.get_city_by_id(2))
-    crud.create_team('Cat Lovers', 'Farm for Fun', crud.get_user_by_id(5),crud.get_sport_by_id(3), crud.get_city_by_id(3))
-    crud.create_team('Shark Eaters', 'Come play on Saturdays!', crud.get_user_by_id(7),crud.get_sport_by_id(1), crud.get_city_by_id(2))
-    crud.create_team('Speedsters', 'Play ball with me on Tuesday nights!', crud.get_user_by_id(10),crud.get_sport_by_id(2), crud.get_city_by_id(1))
-    crud.create_team('Croissant Eaters', 'Speedsters! Playing every weekend!', crud.get_user_by_id(20),crud.get_sport_by_id(3), crud.get_city_by_id(3))
-    for num in range(40):
-        phone=fake.phone_number()
-        sport = crud.get_sport_by_id(randint(1,len(sports)))
-        city = crud.get_city_by_id(randint(1,len(sports)))
-        crud.create_team_player(phone, crud.get_user_by_id(num), crud.get_team_by_id(randint(1,len(crud.get_teams()))))
     
+
+
+    crud.create_team('Killers', 'Play to win', crud.get_user_by_id(1), crud.get_sport_by_id(1), crud.get_city_by_id(1))
+    for num in range(1, 4):
+        phone = fake.phone_number()
+        new_player = crud.create_team_player(phone, crud.get_user_by_id(num), crud.get_team_by_id(1))
+    
+            
+    crud.create_team('Wombats', 'Best D, greatest offense', crud.get_user_by_id(4),crud.get_sport_by_id(2), crud.get_city_by_id(2))
+    for num in range(4, 10):
+        phone = fake.phone_number()
+        new_player = crud.create_team_player(phone, crud.get_user_by_id(num), crud.get_team_by_id(2))
+
+    crud.create_team('Cougars', 'Come play on Saturdays!', crud.get_user_by_id(10),crud.get_sport_by_id(1), crud.get_city_by_id(2))
+    for num in range(10,19):
+        phone = fake.phone_number()
+        new_player = crud.create_team_player(phone, crud.get_user_by_id(num), crud.get_team_by_id(3))
+
+    crud.create_team('Wolves', 'Play ball with me on Tuesday nights!', crud.get_user_by_id(19),crud.get_sport_by_id(2), crud.get_city_by_id(1))
+    for num in range(19, 25):
+        phone = fake.phone_number()
+        new_player = crud.create_team_player(phone, crud.get_user_by_id(num), crud.get_team_by_id(4))
+
+    crud.create_team('Sharks', 'Speedsters! Playing every weekend!', crud.get_user_by_id(25),crud.get_sport_by_id(3), crud.get_city_by_id(3))
+    for num in range(25, 29):
+        phone = fake.phone_number()
+        new_player = crud.create_team_player(phone, crud.get_user_by_id(num), crud.get_team_by_id(5))
+
+    crud.create_team('Banana Slugs', 'Play to win',crud.get_user_by_id(29), crud.get_sport_by_id(1), crud.get_city_by_id(1))
+    for num in range(29, 36):
+        phone = fake.phone_number()
+        new_player = crud.create_team_player(phone, crud.get_user_by_id(num), crud.get_team_by_id(6))
+
+    crud.create_team('Peach Eaters', 'Best D, greatest offense', crud.get_user_by_id(35),crud.get_sport_by_id(2), crud.get_city_by_id(2))
+    for num in range(35, 36):
+        phone = fake.phone_number()
+        new_player = crud.create_team_player(phone, crud.get_user_by_id(num), crud.get_team_by_id(7))
+
+    crud.create_team('Cat Lovers', 'Farm for Fun', crud.get_user_by_id(36),crud.get_sport_by_id(3), crud.get_city_by_id(3))
+    for num in range(36, 37):
+        phone = fake.phone_number()
+        new_player = crud.create_team_player(phone, crud.get_user_by_id(num), crud.get_team_by_id(8))
+
+    crud.create_team('Shark Eaters', 'Come play on Saturdays!', crud.get_user_by_id(37), crud.get_sport_by_id(1), crud.get_city_by_id(2))
+    for num in range(37, 38):
+        phone = fake.phone_number()
+        new_player = crud.create_team_player(phone, crud.get_user_by_id(num), crud.get_team_by_id(9))
+
+    crud.create_team('Speedsters', 'Play ball with me on Tuesday nights!', crud.get_user_by_id(38),crud.get_sport_by_id(2), crud.get_city_by_id(1))
+    for num in range(38, 39):
+        phone = fake.phone_number()
+        new_player = crud.create_team_player(phone, crud.get_user_by_id(num), crud.get_team_by_id(10))
+
+    crud.create_team('Croissant Eaters', 'Speedsters! Playing every weekend!', crud.get_user_by_id(39),crud.get_sport_by_id(3), crud.get_city_by_id(3))
+    for num in range(39, 41):
+        phone = fake.phone_number()
+        new_player = crud.create_team_player(phone, crud.get_user_by_id(num), crud.get_team_by_id(11))
+
     return
 
 #can either load in test seed data in the games database or call the function to just load
