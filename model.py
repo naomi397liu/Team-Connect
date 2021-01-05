00d208127ae52d04799a9dd628b5822c3ecf26fe
Naomi Liu <nhliu@ucdavis.edu>
2021-01-05 02:31:09 +0000

    comment out track SQLalchemy modifications

--- model.py
+++ model.py
@@ -93,7 +93,7 @@ class Player(db.Model):
 def connect_to_db(flask_app, db_uri='postgresql:///games', echo=True):
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     flask_app.config['SQLALCHEMY_ECHO'] = echo
-    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.app = flask_app #connects db to server
     db.init_app(flask_app) #initalize with flask app
