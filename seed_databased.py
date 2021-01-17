0a2a2737ede15eafff507153582c23ee9a2c289e
Naomi Liu <nhliu@ucdavis.edu>
2021-01-17 00:31:47 +0000

    remove os.system()

--- seed_databased.py
+++ seed_databased.py
@@ -9,8 +9,8 @@ import crud
 from model import db, connect_to_db
 import server
 
-os.system('dropdb games')
-os.system('createdb games')
+# os.system('dropdb games')
+# os.system('createdb games')
 
 connect_to_db(server.app)
 # db.drop_all()
